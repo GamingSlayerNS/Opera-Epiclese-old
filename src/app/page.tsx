2c4b48624a909bf7a137ae3751b61385bdac3da3
GamingSlayerNS <naxelalejandro@hotmail.com>
2023-11-23 02:18:13 -0600

    bruh re-activate user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,11 +28,11 @@ export default function Home() {
             </div>
 
             <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-                {/* <Link 
+                <Link 
                     href='/user/600169846'
                 >
                     User
-                </Link> */}
+                </Link>
                 {/* <Button /> */}
             </div>
         </main>
--- src/app/user/[user]/page.tsx
+++ src/app/user/[user]/page.tsx
@@ -1,77 +1,77 @@
-// import Furina from '@/components/furina'
-// import { EnkaClient } from 'enka-network-api';
-// import Link from 'next/link';
+import Furina from '@/components/furina'
+import { EnkaClient } from 'enka-network-api';
+import Link from 'next/link';
 
-// // Change the directory to store cache data.
-// // Default directory is node_modules/enka-network-api/cache.
-// const enka = new EnkaClient({ cacheDirectory: "./cache", showFetchCacheLog: false, defaultLanguage: "en" });
-// enka.cachedAssetsManager.cacheDirectorySetup();
+// Change the directory to store cache data.
+// Default directory is node_modules/enka-network-api/cache.
+const enka = new EnkaClient({ cacheDirectory: "./cache", showFetchCacheLog: false, defaultLanguage: "en" });
+enka.cachedAssetsManager.cacheDirectorySetup();
 
-// const characters = enka.getAllCharacters();
-// // print character names in language "en"
-// // console.log(characters.map((c: any) => c.name.get("en")));
+const characters = enka.getAllCharacters();
+// print character names in language "en"
+// console.log(characters.map((c: any) => c.name.get("en")));
 
 
 
-// export default function User({ params }: any) {
-//     const user = params.user;
+export default function User({ params }: any) {
+    const user = params.user;
 
-//     // const renderCharaList = (): any => {
-//     //     return characters.map((c: any) => {
-//     //         return <p key={c.name}>{c.name.get()}</p>
-//     //     })
-//     // }
+    // const renderCharaList = (): any => {
+    //     return characters.map((c: any) => {
+    //         return <p key={c.name}>{c.name.get()}</p>
+    //     })
+    // }
 
-//     // const renderUserShowcase = (): any => {
-//     //     try {
-//     //         return enka.fetchUser(user).then((user: any) => {
-//     //             // console.log(user.charactersPreview[0].costume._nameId);
-//     //             return user.characters.map((char: any) => {
-//     //                 const name = char.characterData.name.get();
-//     //                 const level = char.level;
-//     //                 const maxLevel = char.maxLevel;
-//     //                 const statsList = char.stats.statProperties.map((stats: any) => {
-//     //                     return ` - ${stats.fightPropName.get()}: ${stats.valueText}`;
-//     //                 });
-//     //                 const artifacts = char.artifacts;
+    // const renderUserShowcase = (): any => {
+    //     try {
+    //         return enka.fetchUser(user).then((user: any) => {
+    //             // console.log(user.charactersPreview[0].costume._nameId);
+    //             return user.characters.map((char: any) => {
+    //                 const name = char.characterData.name.get();
+    //                 const level = char.level;
+    //                 const maxLevel = char.maxLevel;
+    //                 const statsList = char.stats.statProperties.map((stats: any) => {
+    //                     return ` - ${stats.fightPropName.get()}: ${stats.valueText}`;
+    //                 });
+    //                 const artifacts = char.artifacts;
 
-//     //                 // return <p key={name}>
-//     //                 //     {name} - Lv.{level}/{maxLevel}{statsList.map((c: any) => {
-//     //                 //         return <><br key={c} />{c}</>
-//     //                 //     })}
-//     //                 // </p>
+    //                 // return <p key={name}>
+    //                 //     {name} - Lv.{level}/{maxLevel}{statsList.map((c: any) => {
+    //                 //         return <><br key={c} />{c}</>
+    //                 //     })}
+    //                 // </p>
 
-//     //                 return <p key={name}>
-//     //                     {name} - Lv.{level}/{maxLevel}{artifacts.map((arti: any) => {
-//     //                         return <><br key={arti.mainstat} />{arti.mainstat.getMultipliedValue()}</>
-//     //                     })}
-//     //                 </p>
-//     //             })
-//     //         });
-//     //     } catch (error) {
-//     //         console.log(error);
-//     //     }
-//     // }
+    //                 return <p key={name}>
+    //                     {name} - Lv.{level}/{maxLevel}{artifacts.map((arti: any) => {
+    //                         return <><br key={arti.mainstat} />{arti.mainstat.getMultipliedValue()}</>
+    //                     })}
+    //                 </p>
+    //             })
+    //         });
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
-//     return (
-//         <main className="flex min-h-screen flex-col items-center justify-start p-24">
-//             <div>
-//                 <Furina />
-//                 <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
-//                 {enka.fetchUser(user).then((user: any) => {
-//                     return <h1 className="text-5xl text-center">{user.nickname}</h1>
-//                 })}
-//                 </div>
-//             </div>
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-start p-24">
+            <div>
+                <Furina />
+                <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
+                {enka.fetchUser(user).then((user: any) => {
+                    return <h1 className="text-5xl text-center">{user.nickname}</h1>
+                })}
+                </div>
+            </div>
 
-//             <div className="mt-4 grid text-center lg:max-w-6xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left gap-y-8 gap-x-4">
-//                 <Link 
-//                     href='/'
-//                 >
-//                     Home
-//                 </Link>
-//                 {/* {renderUserShowcase()} */}
-//             </div>
-//         </main>
-//     )
-// }
+            <div className="mt-4 grid text-center lg:max-w-6xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left gap-y-8 gap-x-4">
+                <Link 
+                    href='/'
+                >
+                    Home
+                </Link>
+                {/* {renderUserShowcase()} */}
+            </div>
+        </main>
+    )
+}
